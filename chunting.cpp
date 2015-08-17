ffb41a24850ddd65bc863d3c12480211dc2ecb7d
yarr <yarr@debian>
2015-08-17 16:17:24 +0000

    new code

--- chunting.cpp
+++ chunting.cpp
@@ -7,7 +7,10 @@
 #include <algorithm>
 #include <memory>
 #include <queue>
+#include <deque>
+#include <stack>
 #include <sstream>
+#include <stdexcept>
 
 #include <cmath>
 
@@ -22,7 +25,7 @@ namespace tokens {
 	enum token_type {
 		function = 0,
 		delimeter,
-		operand
+		operand // now is only number
 	};
 	
 	bool is_digit(const std::string & s) {
@@ -105,18 +108,135 @@ namespace tokens {
 }
 
 
+namespace schunting {
+	
+	class CSchuntingAlgo {
+	private:
+		void ConstructRPN();
+		
+	public:
+		CSchuntingAlgo(const std::queue<tokens::TOKEN> & t_q): m_tokens(t_q) {
+			ConstructRPN();
+		}
+		virtual ~CSchuntingAlgo () {}
+		
+		virtual bool IsInteger(const tokens::TOKEN & t) const  {
+			return tokens::is_digit(t.val);
+		}
+		virtual bool IsFunction(const tokens::TOKEN & t) const  {
+			return t.type == tokens::function;
+		}
+		virtual bool IsSeparator(const tokens::TOKEN & t) const  {
+			return t.val == std::string(1, ',');
+		}
+		virtual bool IsOperator(const tokens::TOKEN & t) const  {
+			std::string ops ("+-*/");
+			return ops.find(t.val[0]) != ops.npos;
+		}
+		virtual bool IsOpenBracket(const tokens::TOKEN & t) const  {
+			return t.val == std::string(1, '(');
+		}
+		virtual bool IsCloseBracket(const tokens::TOKEN & t) const  {
+			return t.val == std::string(1, ')');
+		}
+		virtual int GetPriority(const tokens::TOKEN & t) const { // now we think, that all our operators are left associative
+			std::string op(1, t.val);
+			return (op == "+" || op == "-") ? 1 : 2; // "+-" - 1; "*/" - 2
+		}
+		
+		std::deque<tokens::TOKEN> GetResults() const {
+			return m_queue;
+		}
+		
+	private:
+		std::queue<tokens::TOKEN> m_tokens;
+		
+		std::stack<tokens::TOKEN> m_stack;
+		std::deque<tokens::TOKEN> m_queue;
+	};
+	
+	void CSchuntingAlgo::ConstructRPN() {
+		// int var = 0;
+		
+		while(!m_tokens.empty()) {
+			tokens::TOKEN tok(m_tokens.front());
+			m_tokens.pop();
+			
+			if (IsInteger(tok)) {
+				m_queue.push_back(tok);
+			}
+			else if (IsFunction(tok)) {
+				m_stack.push_back(tok);
+			}
+			else if (IsSeparator(tok)) {
+				while (!IsOpenBracket(m_stack.top())) {
+					m_queue.push_back(m_stack.top());
+					m_stack.pop();
+				}
+				
+				if (m_stack.empty())
+					throw std::runtime_error("Parse error: is absent func parameter separator (',') or open bracket");
+			}
+			else if (IsOperator(tok)) {
+				while (
+					IsOperator(m_stack.top()) &&
+					(GetPriority(tok) <= GetPriority(m_stack.top()))
+				) {
+					m_queue.push_back(m_stack.top());
+					m_stack.pop();
+				}
+				
+				m_stack.push(tok);
+			}
+			else if (IsOpenBracket(tok)) {
+				m_stack.push(tok);
+			}
+			else if (IsCloseBracket(tok)) {
+				;
+			}
+			//
+		}
+		
+		while(!m_stack.empty()) {
+			
+		}
+		
+		return;
+	}
+	
+	
+	template <typename T>
+	T CalculateRPN (std::deque<tokens::TOKEN> && q) {
+		std::deque<tokens::TOKEN> toks(q);
+		
+		;
+		
+		return T();
+	}
+	
+}
+
+
 int main () {
 	std::string cmds = "(1 + pow(3, 4) + (3 -1)*4) / 3";
 	std::istringstream iss(cmds);
 	tokens::CTokenizer toks (tokens::var_func, tokens::var_delims, iss);
+	
+	/*
 	std::queue<tokens::TOKEN> dat_toks = toks.GetTokens();
-	
-	
 	while(!dat_toks.empty()) {
 		tokens::TOKEN t(dat_toks.front());
 		dat_toks.pop();
 		
 		std::cout << t.type << "; " << t.val << "\n";
+	}
+	*/
+	try {
+		schunting::CSchuntingAlgo chnt(toks.GetTokens());
+		std::cout << "Results: " << schunting::CalculateRPN<long long> (chnt.GetResults()) << std::endl;
+	} catch (std::exception & Exc) {
+		std::cout << "Have caught an exception: " << Exc.what() << "\n";
+		return 1001;
 	}
 	
 	
